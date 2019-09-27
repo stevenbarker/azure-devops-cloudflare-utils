536066e6558cef71dae86a0c573c320b79eaba9d
Steven Barker <stevenbarker@netconstruct.co.uk>
2019-09-27 23:15:03 +0100

    Updated .gitignore

--- tasks/CloudflareAddRemoveOrigin/enums/OriginStatus.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var OriginStatus;
-(function (OriginStatus) {
-    OriginStatus[OriginStatus["Enable"] = 0] = "Enable";
-    OriginStatus[OriginStatus["Disable"] = 1] = "Disable";
-})(OriginStatus = exports.OriginStatus || (exports.OriginStatus = {}));
